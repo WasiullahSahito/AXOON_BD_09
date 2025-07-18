12d108dcdb619a6bd098761ac27d0e536c6d6798
WasiullahSahito <wasi1237585@gmail.com>
2025-07-18 06:30:35 +0500

    Modified

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -81,7 +81,7 @@ class CheckoutController extends Controller
             session()->forget('cart');
             return redirect()->route('checkout.success')->with('success', 'Order placed successfully with Cash on Delivery!');
         } elseif ($request->payment_method === 'stripe') {
-            Stripe::setApiKey(env('STRIPE_SECRET'));
+            Stripe::setApiKey(env('STRIPE_KEY'));
 
             $line_items = [];
             foreach ($cart as $id => $details) {
